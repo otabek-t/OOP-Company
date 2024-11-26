48669b4e1befa8378f5718da7b64e346d34ceb8b
otabek-t <totabek006@gmail.com>
2024-11-26 15:15:42 +0300

    OOP company

---
+++ CompanyService.cs
@@ -0,0 +1,60 @@
+﻿using oopTasks.Model;
+
+namespace oopTasks.userService
+{
+    
+    public class CompanyService
+    {
+        private List<Company> companies;
+        public CompanyService()
+        {
+            companies = new List<Company>();
+        }
+        public Company GetById(Guid id)
+        {
+            foreach (var comp in companies)
+            {
+                if (comp.Id == id)
+                {
+                    return comp;
+                }
+
+            }
+            return null;
+
+        }
+        public Company AddDepartment(Company department)
+        {
+            department.Id = Guid.NewGuid();
+            companies.Add(department);
+            return department;
+        }
+        public bool UpdateDepartment(Company department) 
+        {
+            var updepartment = GetById(department.Id);
+            if (updepartment is null)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        public bool DeleteDepartment(Guid Id)
+        {
+            var deleteDepartment = GetById(Id);
+            if (deleteDepartment is null)
+            {
+               return false;
+            }
+            companies.Remove(deleteDepartment);
+            return true;
+        }
+
+        public List<Company> GetAll() 
+        {
+            return companies;
+        }
+
+    }
+}
